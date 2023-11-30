0f96f2a0deb7e6a1a413c7e69f2045182925583c
Ján Macháček <xmachac5@fi.muni.cz>
2023-11-30 22:09:45 +0100

    feat: structure for whistleblower list

---
+++ app/whistleblowerList/page.tsx
@@ -0,0 +1,12 @@
+const WhistleblowerPage = () => (
+    <div className="flex h-screen items-center justify-center">
+		<p
+          className="mt-6 animate-fade-up text-center text-gray-500 opacity-0 [text-wrap:balance] md:text-xl"
+          style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
+        >
+          Whistleblowerlis
+        </p>
+	</div>
+)
+
+export default WhistleblowerPage;
