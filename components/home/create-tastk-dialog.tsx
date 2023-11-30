3c281d6138d31c2d1932fb12b6e005bd87ea56da
Ján Macháček <xmachac5@fi.muni.cz>
2023-11-30 22:08:54 +0100

    feat: basic dialog

---
+++ components/home/create-tastk-dialog.tsx
@@ -0,0 +1,47 @@
+'use client';
+import { useRef, useState } from 'react';
+
+export const CreateTaskDialog = () => {
+	const dailogRef = useRef<HTMLDialogElement>(null);
+	const [value, setValue] = useState('Plný počet bodov');
+
+	return (
+		<>
+			<button
+				type="button"
+				className="rounded-full border border-black bg-black p-1.5 px-4 text-sm text-white transition-all hover:bg-white hover:text-black"
+				onClick={() => {
+					dailogRef.current?.show();
+				}}
+                style={{ animationDelay: "0.20s", animationFillMode: "forwards" }}
+			>
+				File a report
+			</button>
+			<dialog
+				ref={dailogRef}
+				className="h-full w-full items-center justify-center bg-[transparent] backdrop:backdrop-blur-md [&[open]]:flex"
+			>
+				<form className="flex flex-col gap-3 border border-black bg-indigo-50 p-6">
+					<h2 className="text-xl">Crime report:</h2>
+					{/* TODO: Use state to control this input */}
+					<textarea
+						className="border-0 border-b-2 bg-transparent p-3"
+						value={value}
+						onChange={e => setValue(e.target.value)}
+					/>
+					<button
+						className="rounded-full border border-black bg-black p-1.5 px-4 text-sm text-white transition-all hover:bg-white hover:text-black"
+						onClick={e => {
+							e.preventDefault();
+							console.log(value);
+							setValue('Plný počet bodov');
+							dailogRef.current?.close();
+						}}
+					>
+						Send
+					</button>
+				</form>
+			</dialog>
+		</>
+	);
+};
