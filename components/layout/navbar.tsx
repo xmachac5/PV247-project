c9b70a4f385b37038433dd98453e051721b54814
Ján Macháček <xmachac5@fi.muni.cz>
2023-11-30 22:09:57 +0100

    feat: add whistleblower list to nav

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -6,22 +6,24 @@ import useScroll from "@/lib/hooks/use-scroll";
 import { useSignInModal } from "./sign-in-modal";
 import UserDropdown from "./user-dropdown";
 import { Session } from "next-auth";
+import { usePathname } from 'next/navigation';
 
 export default function NavBar({ session }: { session: Session | null }) {
   const { SignInModal, setShowSignInModal } = useSignInModal();
   const scrolled = useScroll(50);
+  const pathname = usePathname();
 
   return (
     <>
       <SignInModal />
       <div
-        className={`fixed top-0 w-full flex justify-center ${
+        className={`fixed top-0 flex w-full justify-center ${
           scrolled
             ? "border-b border-gray-200 bg-white/50 backdrop-blur-xl"
             : "bg-white/0"
         } z-30 transition-all`}
       >
-        <div className="mx-5 flex h-16 max-w-screen-xl items-center justify-between w-full">
+        <div className="mx-5 flex h-16 w-full max-w-screen-xl items-center justify-between">
           <Link href="/" className="flex items-center font-display text-2xl">
             <Image
               src="/logo.png"
@@ -33,6 +35,13 @@ export default function NavBar({ session }: { session: Session | null }) {
             <p>Informer</p>
           </Link>
           <div>
+            <Link
+              prefetch={false}
+              className={`link ${pathname === "/whistleblowerList" ? "active" : ""} rounded-full border border-black bg-black p-1.5 px-4 text-sm text-white transition-all hover:bg-white hover:text-black`}
+              href="/whistleblowerList"
+            >
+              Tickets
+            </Link>
             {session ? (
               <UserDropdown session={session} />
             ) : (
