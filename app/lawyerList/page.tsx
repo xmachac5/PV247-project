2503622f42858f976f331412d290c203a7870b6b
Ján Macháček <xmachac5@fi.muni.cz>
2023-11-30 22:21:07 +0100

    fix: lawyerpage

---
+++ app/lawyerList/page.tsx
@@ -0,0 +1,12 @@
+const LawyerPage = () => (
+    <div className="flex h-screen items-center justify-center">
+		<p
+          className="mt-6 animate-fade-up text-center text-gray-500 opacity-0 [text-wrap:balance] md:text-xl"
+          style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
+        >
+          LawyerPage
+        </p>
+	</div>
+)
+
+export default LawyerPage;
