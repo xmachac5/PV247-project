b3d5634bfd1ee96dd6a0544e9cc4aa58475b2b76
Ján Macháček <xmachac5@fi.muni.cz>
2023-12-09 20:18:55 +0100

    feat: list first visual

--- app/whistleblowerList/page.tsx
+++ app/whistleblowerList/page.tsx
@@ -1,12 +1,24 @@
-const WhistleblowerPage = () => (
-    <div className="flex h-screen items-center justify-center">
-		<p
-          className="mt-6 animate-fade-up text-center text-gray-500 opacity-0 [text-wrap:balance] md:text-xl"
-          style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
-        >
-          Whistleblowerlis
-        </p>
-	</div>
-)
+import Link from "next/link";
+
+const WhistleblowerPage = () => {
+	const data = [{"id": 1, "title": "test1"},{ "id": 2, "title": "test2"}]
+
+	return (
+		<div className="flex h-screen items-center justify-center">
+			{data.map(todo => (
+				<div
+					key={todo.id}
+					className="flex items-center justify-between px-4 py-2 odd:bg-slate-100"
+				>
+					{/* TODO: To be links */}
+					<div>{todo.title}</div>
+					<Link href="/[id]" as={`/${todo.id}`}>
+						<p>View Details</p>
+					</Link>
+				</div>
+			))}
+		</div>
+	);
+};
 
 export default WhistleblowerPage;
