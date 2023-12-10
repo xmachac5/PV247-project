ccedc4a2c1790f052657d93298548b6c9e82863e
Ján Macháček <xmachac5@fi.muni.cz>
2023-12-10 17:39:11 +0100

    feat: loading ticket list

--- app/ticketList/loading.tsx
+++ app/ticketList/loading.tsx
@@ -1,6 +1,6 @@
 const LoadingPage = () => (
 	<div className="flex h-screen items-center justify-center">
-		<div className="h-12 w-12 animate-spin rounded-full border-r-4 border-t-4 bg-blue-400" />
+		<div className="h-12 w-12 animate-spin rounded-full border-r-4 border-t-4 bg-cyan-500" />
 	</div>
 );
 
