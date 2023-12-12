f8d57a41deb33c0895becca1e9e53ef9606b5b72
Matej Hrica <mtjhrc@gmail.com>
2023-12-12 05:13:06 +0100

    Fix compilation

--- app/api/report/[id]/route.ts
+++ app/api/report/[id]/route.ts
@@ -3,7 +3,7 @@ import { reportSchema } from '@/app/model/report';
 
 export const GET = async (
   req: Request,
-  { params }: { id: { id: string } }
+  { params }: { params: { id: string } }
 ) => { 
     const report = await prisma.report.findFirst({
       where: {
