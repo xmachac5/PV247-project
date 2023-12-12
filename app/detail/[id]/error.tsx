1d68e1345cc7bd3ca66a8022f6a968a7e2cbd55a
xmachac5 <493237@mail.muni.cz>
2023-12-12 11:42:39 +0100

    feat: report loading and error

---
+++ app/detail/[id]/error.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+const ErrorgPage = () => (
+	<div className="flex h-screen items-center justify-center">
+		<div className="text-3xl text-red-500">Data failed to load</div>
+	</div>
+);
+
+export default ErrorgPage;
