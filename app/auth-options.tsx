675a3c56c4cd03601c4d55f95a4e019b58b626d9
xmachac5 <493237@mail.muni.cz>
2023-12-12 11:39:31 +0100

    feat: login

--- app/auth-options.tsx
+++ app/auth-options.tsx
@@ -8,7 +8,7 @@ declare module 'next-auth' {
     user: {
       id: string;
       name: string;
-      email:string;
+      email: string;
     }
   }
 }
@@ -35,24 +35,31 @@ export const authOptions: NextAuthOptions = {
       // e.g. domain, username, password, 2FA token, etc.
       // You can pass any HTML attribute to the <input> tag through the object.
       credentials: {
-        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        email: { label: "Email", type: "text", placeholder: "jsmith" },
         password: { label: "Password", type: "password" }
       },
       async authorize(credentials, req) {
-        // Add logic here to look up the user from the credentials supplied
-        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
-        console.log(user);
-        console.log(credentials);
+        try {
+          const user = await prisma.user.findFirst({
+            where: {
+                password: credentials?.password,
+                email: credentials?.email,
+              },
+            });
+          console.log(user);
 
-        if (user) {
-          // Any object returned will be saved in `user` property of the JWT
-          console.log("here");
-          return user;
-        } else {
-          // If you return null then an error will be displayed advising the user to check their details.
-          return null
+          if (user) {
+            // Any object returned will be saved in `user` property of the JWT
+            console.log("here");
+            return user;
+          } else {
+            // If you return null then an error will be displayed advising the user to check their details.
+            return { id: "1", name: "J Smith", email: "jsmith@example.com" };
 
-          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+            // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+          }
+        } catch (error) {
+          return null;
         }
       }
     }),
