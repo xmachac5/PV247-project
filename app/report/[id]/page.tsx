978e4ad9707bfa2b9c0bf2c9f6e9ade0c826e54b
Matej Hrica <mtjhrc@gmail.com>
2023-12-12 14:13:14 +0100

    Implement report page

--- app/report/[id]/page.tsx
+++ app/report/[id]/page.tsx
@@ -20,7 +20,7 @@ const Page = async ({ params, searchParams }: Props) => {
   });
 
   if (searchParams.pasw == null) {
-    return <RedirectReplaceUrl url={"/?reportid="+params.id} />
+    return <RedirectReplaceUrl url={"/?reportid=" + params.id} />;
   }
 
   let content: any = "failed to load";
@@ -31,10 +31,72 @@ const Page = async ({ params, searchParams }: Props) => {
   } else if (report.password !== searchParams.pasw) {
     content = <p className="font-bold text-red-600">Invalid password.</p>;
   } else {
-    //TODO: actually render it!
-    content = <div>{JSON.stringify(report)}</div>;
+    const personalData = report.personalData;
+    content = (
+      <div className="flex flex-col sm:flex-row">
+        {report && (
+          <div className="inline-block m-3 rounded-lg border bg-white p-4 shadow-md">
+            <h3 className="mb-4 from-black to-stone-500 text-2xl">
+              {report.title}
+            </h3>
+            <table>
+              <tr>
+                <th>Id:</th>
+                <td>{report.id}</td>
+              </tr>
+              <tr>
+                <th>Title:</th>
+                <td>{report.title}</td>
+              </tr>
+              <tr>
+                <th>Created at:</th>
+                <td>{report.createdAt.toLocaleDateString()}</td>
+              </tr>
+              <tr>
+                <th>Incident occured at:</th>
+                <td>{report.datetime.toLocaleDateString()}</td>
+              </tr>
+              <tr>
+                <th>Reason:</th>
+                <td>{report.reason}</td>
+              </tr>
+              <tr>
+                <th>Description:</th>
+                <td>{report.description}</td>
+              </tr>
+              <tr>
+                <th>State:</th>
+                <td>{report.state}</td>
+              </tr>
+              <tr>
+                <th>Report type:</th>
+                <td>{report.reportType?.name ?? ""}</td>
+              </tr>
+            </table>
+          </div>
+        )}
+        <div className="m-3 inline-block rounded-lg border bg-white p-4 shadow-md">
+          <h3 className="mb-4 from-black to-stone-500 text-2xl">
+            Whistleblower Information
+          </h3>
+          {personalData == undefined ? (
+            <p className="mb-2 text-lg">
+              Anonymous Whistleblower {JSON.stringify(report.personalData)}
+            </p>
+          ) : (
+            <>
+              <p className="mb-2 text-lg">{`Name: ${personalData.name}`}</p>
+              <p className="mb-2 text-lg">{`Birthdate: ${personalData.dateBorn}`}</p>
+              <p className="mb-2 text-lg">{`Address: ${personalData.address}`}</p>
+              <p className="mb-2 text-lg">{`Email: ${personalData.email}`}</p>
+              <p className="mb-2 text-lg">{`Phone: ${personalData.phone}`}</p>
+            </>
+          )}
+        </div>
+      </div>
+    );
   }
-  
+
   return (
     <div
       className="flex animate-fade-up flex-col"
